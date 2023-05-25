c957f32320b0ab69a12ae549b3d6a4ec1ad4f479
Niko Birbilis <nikob381@gmail.com>
2023-05-25 15:28:47 -0400

    feat(sitetitle): add SiteTitle component

---
+++ components/SiteTitle.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
+
+export default function SiteTitle({ home, name }) {
+  return home ? (
+    <>
+      <Image
+        priority
+        src="/niko-here/images/profile.jpeg"
+        className={utilStyles.borderCircle}
+        height={144}
+        width={144}
+        alt=""
+      />
+      <h1 className={utilStyles.heading2Xl}>{name}</h1>
+    </>
+  ) : (
+    <>
+      <Link href="/">
+        <Image
+          priority
+          src="/niko-here/images/profile.jpeg"
+          className={utilStyles.borderCircle}
+          height={108}
+          width={108}
+          alt=""
+        />
+      </Link>
+      <h2 className={utilStyles.headingLg}>
+        <Link href="/" className={utilStyles.colorInherit}>
+          {name}
+        </Link>
+      </h2>
+    </>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import SiteTitle from "./SiteTitle";
 
 const name = "Niko Here";
 export const siteTitle = "Niko Here";
@@ -26,37 +25,7 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/niko-here/images/profile.jpeg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/niko-here/images/profile.jpeg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
+        <SiteTitle home={home} name={name} />
       </header>
       <main>{children}</main>
       {!home && (
