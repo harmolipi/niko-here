d0741e4f2c2c4130230f460afd1a34e5692d3418
Niko Birbilis <nikob381@gmail.com>
2023-05-25 00:40:40 -0400

    feat(layout): change site name to "Niko Here"

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Niko";
-export const siteTitle = "Next.js Sample Website";
+const name = "Niko Here";
+export const siteTitle = "Niko Here";
 
 export default function Layout({ children, home }) {
   return (
