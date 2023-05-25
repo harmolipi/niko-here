a558082e6d9681552b0dc66091a2bf7a0c89edf2
Niko Birbilis <nikob381@gmail.com>
2023-05-25 15:39:54 -0400

    feat(blogpost): add BlogPost component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import Date from "../../components/date";
-import utilStyles from "../../styles/utils.module.css";
+import BlogPost from "../../components/BlogPost";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -27,13 +26,7 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
-      </article>
+      <BlogPost postData={postData} />
     </Layout>
   );
 }
