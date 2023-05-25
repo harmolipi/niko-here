eba1da1a487afda6b607f1194980ada5823fe64d
Niko Birbilis <nikob381@gmail.com>
2023-05-25 15:32:23 -0400

    feat(bloglist): add BlogList component

---
+++ components/BlogList.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Date from "./date";
+import utilStyles from "../styles/utils.module.css";
+
+export default function BlogList({ posts }) {
+  return (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {posts.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import Link from "next/link";
-import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import BlogList from "../components/BlogList";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -33,20 +32,7 @@ export default function Home({ allPostsData }) {
           , WordPress, and my wonderful family.
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <BlogList posts={allPostsData} />
     </Layout>
   );
 }
