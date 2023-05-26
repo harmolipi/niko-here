faa8d7481a4a3a8b3d5cc6b79b97e34d1af6a761
Niko Birbilis <nikob381@gmail.com>
2023-05-26 01:24:29 -0400

    feat(date): add ability to name years by adding to the `namedYears` object

--- components/date.js
+++ components/date.js
@@ -1,6 +1,16 @@
 import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
+  const namedYears = {
+    2023: "The Year Lucia Was Born",
+  };
+
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  const year = namedYears[date.getFullYear()] || date.getFullYear();
+  return (
+    <time dateTime={dateString}>
+      {format(date, "MMMM do, ")}
+      {year}
+    </time>
+  );
 }
