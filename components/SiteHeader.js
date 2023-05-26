2dd9dcacc9470654126669ec8ce1eb8f2c9779a4
Niko Birbilis <nikob381@gmail.com>
2023-05-26 01:09:00 -0400

    feat(layout): add homepage hero image

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,18 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 import utilStyles from "../styles/utils.module.css";
+import styles from "./SiteHeader.module.css";
 
 export default function SiteHeader({ home, name }) {
   return home ? (
     <>
-      <Image
-        priority
-        src="/niko-here/images/profile.jpeg"
-        className={utilStyles.borderCircle}
-        height={144}
-        width={144}
-        alt=""
-      />
+      <div className={styles.hero}>
+        <div className={styles.heroContainer}>
+          <Image
+            priority
+            src="/niko-here/images/home-hero.webp"
+            fill={true}
+            alt="Garden of Versailles by John Vanderlyn"
+            title="Garden of Versailles by John Vanderlyn"
+            className={styles.heroImage}
+          />
+        </div>
+        <Image
+          priority
+          src="/niko-here/images/profile.jpeg"
+          className={`${utilStyles.borderCircle} ${styles.heroProfile}`}
+          height={144}
+          width={144}
+          alt=""
+        />
+      </div>
       <h1 className={utilStyles.heading2Xl}>{name}</h1>
     </>
   ) : (
