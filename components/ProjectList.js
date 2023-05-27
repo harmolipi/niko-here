4b7e3cbc8b6b3334aed2cee38672d5c695fcccb6
Niko Birbilis <nikob381@gmail.com>
2023-05-27 18:45:56 -0400

    feat(projectlist): add ProjectList component

---
+++ components/ProjectList.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
+
+export default function ProjectList({ projects, embedded = false }) {
+  return (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {embedded && <h2 className={utilStyles.headingLg}>Projects</h2>}
+      <ul className={utilStyles.list}>
+        {projects.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            <Link href={`/projects/${id}`}>{title}</Link>
+            <br />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
